1600a28e364219b178e6abdaeb1b70b247c48900
Mithun Halder <mithun@infodigita.com>
2023-04-04 12:14:02 +0600

    FreightCosts Updated

--- config/freightCosts.php
+++ config/freightCosts.php
@@ -1,5 +1,62 @@
 <?php
 
+// old
+// return [
+//     [
+//         'sender_country'        =>  'CH',
+//         'receiver_country'      =>  'CH',
+//         'currency'              =>  'CHF',
+//         'weight_unit'           =>  'kg',
+//         'min_weight'            =>  '5.00',
+//         'min_weight_fee'        =>  '13.50',
+//         'additional_weight_fee' =>  '1.00',
+//         'import_customs_fee'    =>  '0.00',
+//         'stop_fee'              =>  '15.00',
+//         'fuel_surcharge'        =>  '0.00'
+//     ],
+
+//     [
+//         'sender_country'        =>  'CH',
+//         'receiver_country'      =>  'DE',
+//         'currency'              =>  'CHF',
+//         'weight_unit'           =>  'kg',
+//         'min_weight'            =>  '5.00',
+//         'min_weight_fee'        =>  '25.50',
+//         'additional_weight_fee' =>  '1.50',
+//         'import_customs_fee'    =>  '38.00',
+//         'stop_fee'              =>  '15.00',
+//         'fuel_surcharge'        =>  '13.00'
+//     ],
+
+//     [
+//         'sender_country'        =>  'DE',
+//         'receiver_country'      =>  'DE',
+//         'currency'              =>  'CHF',
+//         'weight_unit'           =>  'kg',
+//         'min_weight'            =>  '5.00',
+//         'min_weight_fee'        =>  '12.50',
+//         'additional_weight_fee' =>  '1.00',
+//         'import_customs_fee'    =>  '0.00',
+//         'stop_fee'              =>  '15.00',
+//         'fuel_surcharge'        =>  '13.00'
+//     ],
+
+//     [
+//         'sender_country'        =>  'DE',
+//         'receiver_country'      =>  'CH',
+//         'currency'              =>  'CHF',
+//         'weight_unit'           =>  'kg',
+//         'min_weight'            =>  '5.00',
+//         'min_weight_fee'        =>  '25.50',
+//         'additional_weight_fee' =>  '1.50',
+//         'import_customs_fee'    =>  '38.00',
+//         'stop_fee'              =>  '15.00',
+//         'fuel_surcharge'        =>  '13.00'
+//     ],
+// ];
+
+
+//New
 return [
     [
         'sender_country'        =>  'CH',
@@ -7,10 +64,10 @@ return [
         'currency'              =>  'CHF',
         'weight_unit'           =>  'kg',
         'min_weight'            =>  '5.00',
-        'min_weight_fee'        =>  '13.50',
-        'additional_weight_fee' =>  '1.00',
+        'min_weight_fee'        =>  '15.50',
+        'additional_weight_fee' =>  '2.00',
         'import_customs_fee'    =>  '0.00',
-        'stop_fee'              =>  '15.00',
+        'stop_fee'              =>  '17.00',
         'fuel_surcharge'        =>  '0.00'
     ],
 
@@ -20,11 +77,11 @@ return [
         'currency'              =>  'CHF',
         'weight_unit'           =>  'kg',
         'min_weight'            =>  '5.00',
-        'min_weight_fee'        =>  '25.50',
-        'additional_weight_fee' =>  '1.50',
-        'import_customs_fee'    =>  '38.00',
-        'stop_fee'              =>  '15.00',
-        'fuel_surcharge'        =>  '13.00'
+        'min_weight_fee'        =>  '27.50',
+        'additional_weight_fee' =>  '2.50',
+        'import_customs_fee'    =>  '40.00',
+        'stop_fee'              =>  '17.00',
+        'fuel_surcharge'        =>  '15.00'
     ],
 
     [
@@ -33,11 +90,11 @@ return [
         'currency'              =>  'CHF',
         'weight_unit'           =>  'kg',
         'min_weight'            =>  '5.00',
-        'min_weight_fee'        =>  '12.50',
-        'additional_weight_fee' =>  '1.00',
+        'min_weight_fee'        =>  '14.50',
+        'additional_weight_fee' =>  '2.00',
         'import_customs_fee'    =>  '0.00',
-        'stop_fee'              =>  '15.00',
-        'fuel_surcharge'        =>  '13.00'
+        'stop_fee'              =>  '17.00',
+        'fuel_surcharge'        =>  '15.00'
     ],
 
     [
@@ -46,10 +103,10 @@ return [
         'currency'              =>  'CHF',
         'weight_unit'           =>  'kg',
         'min_weight'            =>  '5.00',
-        'min_weight_fee'        =>  '25.50',
-        'additional_weight_fee' =>  '1.50',
-        'import_customs_fee'    =>  '38.00',
-        'stop_fee'              =>  '15.00',
-        'fuel_surcharge'        =>  '13.00'
+        'min_weight_fee'        =>  '27.50',
+        'additional_weight_fee' =>  '2.50',
+        'import_customs_fee'    =>  '40.00',
+        'stop_fee'              =>  '17.00',
+        'fuel_surcharge'        =>  '15.00'
     ],
 ];
